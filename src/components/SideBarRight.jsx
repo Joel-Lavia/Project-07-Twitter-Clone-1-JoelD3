8b8e6c6c58089ff0e3feb45a5acff8e7c16384e4
Joel-Lavia <joeldisidi8@gmail.com>
2023-12-07 01:40:42 +0100

    integration de la list de followers

--- src/components/SideBarRight.jsx
+++ src/components/SideBarRight.jsx
@@ -1,51 +1,65 @@
 import data from "../Models/Data";
-import settingimg from "../icons/Settings.png"
-import seachIcon from "../icons/Search.png"
+import settingimg from "../icons/Settings.png";
+import seachIcon from "../icons/Search.png";
 import cnn from "../images/tweet-profile-photo.png";
 import nytimes from "../images/Tweet2.png";
 import twetterMascote from "../images/MascoteImg.png";
-
 import Trading from "./Trading";
 import ProfilSetting from "./ProfilSetting";
 function SideBarRight() {
-const tweetFollow = data.map((allNameTweet) => {
-   return(
-      <div key={allNameTweet.id}>
-      <img src={allNameTweet.avatar} alt="" />
-      <p>{allNameTweet.nomTweet}</p>
-      <p>{allNameTweet.lienTweet}</p>
-     <button className="button">Follow</button>
-   
+  const tweetFollow = data.map((allNameTweet) => {
+    return (
+      <div key={allNameTweet.id} className="followContent">
+         
+         <div className="follow">
+         <img src={allNameTweet.avatar} alt="" />
+         <p>{allNameTweet.nomTweet} <i><img src={allNameTweet.certification} alt="" /></i></p>
+         </div>
+
+     <div>
+     <button className="button" >Follow</button>
+     </div>
+
       </div>
-   );
-})
-return(
- <section className="seconSideBare">
-    <form action="" className="seach">
-    <i><img src={seachIcon} alt="" /></i> <input  type="seach" placeholder="Serach Twitter" /> 
-    </form>
-    <div className="trading">
-    <h2>Trends for you <i><img src={settingimg} alt="" /></i> </h2>
-    <Trading/>
-    <Trading/>
-    <Trading/>
-    <Trading/>
-    <div>
-    <button className="btnShowmore">Show more</button>
-    </div>
-    </div>
+    );
+  });
+  return (
+    <section className="seconSideBare">
 
-    <div>
-    
-    <div className="follow">
-    {tweetFollow.shift()}
-    {tweetFollow.shift()}
-    {tweetFollow.shift()}
-    </div>
+      <form action="" className="seach">
+        <i>
+          <img src={seachIcon} alt="" />
+        </i>{" "}
+        <input type="seach" placeholder="Serach Twitter" />
+      </form>
 
-    </div>
-    
- </section>
-); 
+      <div className="trading">
+        <h2>
+          Trends for you{" "}
+          <i>
+            <img src={settingimg} alt="" />
+          </i>{" "}
+        </h2>
+        <Trading />
+        <Trading />
+        <Trading />
+        <Trading />
+        <div>
+          <button className="btnShowmore">Show more</button>
+        </div>
+      </div>
+
+      <div className="foolowList">
+        
+         {tweetFollow.shift()}
+         {tweetFollow.shift()}
+         {tweetFollow.shift()}
+         <div>
+          <button className="btnShowmore">Show more</button>
+        </div>
+      </div>
+      
+    </section>
+  );
 }
-export default SideBarRight
+export default SideBarRight;
